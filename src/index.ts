73505dfe36e702300c21a73a83f6685a5c160e53
Josh Harris <js.harris711@gmail.com>
2026-02-24 22:49:15 -0500

    quick app to get ejs runnign in elysia js

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import { Elysia } from "elysia";
+import ejs from 'ejs'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .onAfterHandle(({response, set }) => {
+    if (typeof response === 'string' && response.trim().startsWith('<')) {
+      set.headers['content-type'] = 'text/html; charset=utf-8';
+    }
+
+    return response;
+  })
+  .get("/", () => "Hi Elysia")
+  .get("/home", async () => {
+    const html = await ejs.renderFile('views/home.ejs', { name: 'Elysia' });
+    return html
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
